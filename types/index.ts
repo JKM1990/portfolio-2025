886393e527772bc0277712de771e319b40abb4df
jkm <jeffkmanser@gmail.com>
2025-04-01 23:16:25 -0300

    add ts interfaces for data models

---
+++ types/index.ts
@@ -0,0 +1,38 @@
+export interface Skill {
+    _id?: string;
+    name: string;
+    icon: string;
+  }
+  
+  export interface Technology {
+    name: string;
+  }
+  
+  export interface Project {
+    _id?: string;
+    title: string;
+    description: string;
+    featured: boolean;
+    imageSrc: string;
+    technologies: Technology[];
+    githubLink?: string;
+    liveLink?: string;
+    details?: {
+      problem?: string;
+      solution?: string;
+      challenges?: string[];
+    };
+  }
+  
+  export interface NavLink {
+    path: string;
+    label: string;
+    icon: string;
+    isActive: boolean;
+  }
+  
+  export interface SocialLink {
+    platform: string;
+    url: string;
+    icon: string;
+  }
